08057c9c4aa2a1d77ed4ff9390b56790c0bf0492
qinjx <jianxiang.qin@alibaba-inc.com>
2013-11-22 21:37:46 +0800

    完善【错误使用方法】的测试用例，性能测试和正确使用方法的测试用例还远不完善

--- runtime/BloomFilter/BloomFilter.php
+++ runtime/BloomFilter/BloomFilter.php
@@ -18,6 +18,7 @@ class LtBloomFilter {
 		73,79,83,89,97,101,103,107,109,113
 	);
 	protected $hashFunAmountToUse;
+	protected $hasBeenInitialized = false;
 
 	/**
 	 * 设置待存储的元素数量上限
@@ -28,7 +29,7 @@ class LtBloomFilter {
 		if (is_int($size) && 0 < $size) {
 			$this->bucketSize = $size;
 		} else {
-			trigger_error("Bucket Size must be integer, and greater than 0", E_USER_ERROR);
+			trigger_error("Bucket Size must be integer between 1 and " . PHP_INT_MAX . "(PHP_INT_MAX)", E_USER_ERROR);
 		}
 	}
 
@@ -53,7 +54,9 @@ class LtBloomFilter {
 	 * 以定时将内存中的BitArray存储到硬盘上
 	 */
 	public function setImageFile($filePath) {
-		if (!is_file($filePath)) {
+		if (!is_string($filePath)) {
+			trigger_error("$filePath must be a string", E_USER_ERROR);
+		} else if (!is_file($filePath)) {
 			$dir = dirname($filePath);
 			if (!is_dir($dir)) {
 				if (is_writable(dirname($dir))) {
@@ -91,40 +94,56 @@ class LtBloomFilter {
 	 * m,n,p,k计算关系参见：http://en.wikipedia.org/wiki/Bloom_filter#Probability_of_false_positives
 	 */
 	public function init() {
-		//根据n, p算m
-		$this->bitArrayMaxLength = bcmul("-1",
-			bcmul((string) $this->bucketSize, (string) log($this->errorRate) / pow(log(2), 2))
-		);
+		if ($this->bucketSize) {
+			//根据n, p算m
+			$this->bitArrayMaxLength = bcmul("-1",
+				bcmul((string) $this->bucketSize, (string) log($this->errorRate) / pow(log(2), 2))
+			);
 
-		//根据m,n算k
-		$this->hashFunAmountToUse = ceil((float) bcdiv($this->bitArrayMaxLength, $this->bucketSize) * log(2));
+			//根据m,n算k
+			$this->hashFunAmountToUse = ceil((float) bcdiv($this->bitArrayMaxLength, $this->bucketSize) * log(2));
 
-		//若image file存在，且有上次持久化的内容，读取之
-		if (!$this->imageFile) {
-			$this->setImageFile(sys_get_temp_dir() . DIRECTORY_SEPARATOR . crc32(__FILE__) . ".bloom");
-		} else if (0 < filesize($this->imageFile)) {
-			$this->bitArray = unserialize(file_get_contents($this->imageFile));
+
+			if (!$this->imageFile) {
+				$this->setImageFile(sys_get_temp_dir() . DIRECTORY_SEPARATOR . crc32(__FILE__) . ".bloom");
+			} else if (0 < filesize($this->imageFile)) {//若image file存在，且有上次持久化的内容，读取之
+				$this->loadFromDisk();
+			}
+			$this->hasBeenInitialized = true;
+		} else {
+			trigger_error("Please call setBucketSize() before init()", E_USER_ERROR);
 		}
 	}
 
 	/**
 	 * 加入字串
 	 * @param string $str 要加入的字串
+	 * @return boolean
 	 */
 	public function add($str) {
-		$bitArrayChanged = false;
-		for ($i = 0; $i < $this->hashFunAmountToUse; $i++) {
-			$hash = $this->hash($str, $this->bitArrayMaxLength, $this->magicNumbers[$i]);
-			if (!isset($this->bitArray[$hash])) {
-				$this->bitArray[$hash] = 1;
-				$bitArrayChanged = true;
+		if ($this->hasBeenInitialized) {
+			if (is_string($str)) {
+				$bitArrayChanged = false;
+				for ($i = 0; $i < $this->hashFunAmountToUse; $i++) {
+					$hash = $this->hash($str, $this->bitArrayMaxLength, $this->magicNumbers[$i]);
+					if (!isset($this->bitArray[$hash])) {
+						$this->bitArray[$hash] = 1;
+						$bitArrayChanged = true;
+					}
+				}
+				if ($bitArrayChanged) {
+					$this->syncCounter ++;
+					if ($this->syncCounter >= $this->syncThreshHold) {
+						$this->saveToDisk();
+						$this->syncCounter = 0;
+					}
+				}
+				return true;
+			} else {
+				return false;
 			}
-		}
-		if ($bitArrayChanged) {
-			$this->syncCounter ++;
-			if ($this->syncCounter >= $this->syncThreshHold) {
-				$this->saveToDisk();
-			}
+		} else {
+			trigger_error("Please call init() first", E_USER_ERROR);
 		}
 	}
 
@@ -136,13 +155,17 @@ class LtBloomFilter {
 	 * 而返回true，则表示存在（有极小可能误判）之所以不是【一定存在】，是因为bloom filter算法自身允许一定的false positive
 	 */
 	public function has($str) {
-		for ($i = 0; $i < $this->hashFunAmountToUse; $i++) {
-			$hash = $this->hash($str, $this->bitArrayMaxLength, $this->magicNumbers[$i]);
-			if (!isset($this->bitArray[$hash])) {
-				return false;
+		if (is_string($str)) {
+			for ($i = 0; $i < $this->hashFunAmountToUse; $i++) {
+				$hash = $this->hash($str, $this->bitArrayMaxLength, $this->magicNumbers[$i]);
+				if (!isset($this->bitArray[$hash])) {
+					return false;
+				}
 			}
+			return true;
+		} else {
+			return false;
 		}
-		return true;
 	}
 
 	/**
@@ -171,5 +194,9 @@ class LtBloomFilter {
 	 */
 	protected function saveToDisk() {
 		file_put_contents($this->imageFile, serialize($this->bitArray));
+	}
+
+	protected function loadFromDisk() {
+		$this->bitArray = unserialize(file_get_contents($this->imageFile));
 	}
 }
